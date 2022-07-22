bf69fe5861ae9df714943cdb178c28168013bec5
Mubashir Irfan <mubashir.irfan37@gmail.com>
2022-07-22 15:32:11 +0500

    Chart Data

--- src/app/event-tracking/detail/detail.component.ts
+++ src/app/event-tracking/detail/detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EventTrackingDetailService } from './event-tracking-detail.service';
+import { Chart } from 'chart.js';
 
 export interface IEventData {
   date: string;
@@ -19,21 +20,38 @@ export interface IEventData {
 })
 export class DetailComponent implements OnInit {
   public eventsTableData: any;
+  private chart: any;
+  public chartData: any = [];
+  private graphTotals: {[key: string]: number} = {};
+  public chartOptions =  {
+    responsive: true, // Instruct chart js to respond nicely.
+    maintainAspectRatio: false, // Add to prevent default behaviour of full-width/height 
+  }
   constructor(private eventTrackingDetailService: EventTrackingDetailService) {}
 
   ngOnInit(): void {
-    this.eventTrackingDetailService.getEventsData().subscribe((res) => {
+    this.eventTrackingDetailService.getEventsData().subscribe((res: any) => {
       console.log(res);
       this.eventsTableData = res[0];
-      const graphData: {[key: string]: number} = {};
       this.eventsTableData.forEach((event: IEventData) => {
         const date: string = event.date;
-        if (graphData[date]) {
-          graphData[date]++;
+        if (this.graphTotals[date]) {
+          this.graphTotals[date]++;
         } else {
-          graphData[date] = 1;
+          this.graphTotals[date] = 1;
         }
       });
+      Object.keys(this.graphTotals).forEach((date: string) => {
+        this.chartData.push({date, events: this.graphTotals[date]});
+      });
     });
+    console.log('graph Data', this.chartData)
+    this.drawChart();
+  }
+
+  private drawChart() {
+    var canvas = <HTMLCanvasElement>document.getElementById("rohsProductSummaryChart");
+    var ctx = canvas.getContext("2d");
+
   }
 }
